d1ec3286c3cda79528eb718a3e7b23b979b0cbb9
bottoszw <zhangwei@bottos.org>
2017-11-26 16:25:19 +0800

    modify interface of ai asset contract

--- contracts/contracts/DataAssetRegister/AIDataAssetRegister.sol
+++ contracts/contracts/DataAssetRegister/AIDataAssetRegister.sol
@@ -9,6 +9,7 @@ import "../DataExchangeManager/DataExchangeManager.sol";
 
 
 
+
 contract AIDataAssetRegister {
     using LibString for *;
     using LibInt for *;
@@ -16,28 +17,33 @@ contract AIDataAssetRegister {
     using LibDataAsset for *;
     address    owner; 
 
-    DataExchangeManager dataExchangeAddr;
+    DataExchangeManager public dataExchangeAddr;
+    
     
     mapping(string => LibAIDataAsset.AiDataAssetInfo) aiDataAssetMap;
  
     mapping(address => string[]) aiDataAssetAddrMap;
 
+    mapping(string => string[]) dataRequirementToAssetMap;
 
 
     function AIDataAssetRegister(){
         owner = msg.sender;
     }       
 
-    function aiDataRegist(string _registInfoJson) { 
+    function aiDataRegist(string _registInfoJson) returns (bool){ 
 
         string memory assetSignature = _registInfoJson.getStringValueByKey("assetSignature"); 
+        if (assetSignature.equals("")) return false;
         string  memory assetID = LibID.generateID(assetSignature);
         
         
-        LibAIDataAsset.AiDataAssetInfo aiDataAsset = aiDataAssetMap[assetID];     
+        LibAIDataAsset.AiDataAssetInfo aiDataAsset = aiDataAssetMap[assetID];    
+
+        if (!aiDataAsset.dataAssetInfo.assetID.equals("")) return false;
 
         bool result = LibAIDataAsset.jsonParse(aiDataAsset, _registInfoJson);
-        if (false == result) return;
+        if (false == result) return false;
 
         aiDataAsset.dataAssetInfo.assetID = assetID;
 
@@ -46,7 +52,13 @@ contract AIDataAssetRegister {
         owernAllAsset.push(assetID);
 
         string memory dateRequirementID = _registInfoJson.getStringValueByKey("dataRequirementID");        
-        dataExchangeAddr.addDataExchange(owner, owner, dateRequirementID, assetID);
+        dataExchangeAddr.addDataExchange(owner, owner, aiDataAsset.dataAssetInfo.dataRequirementID, assetID);
+
+        
+        string[] requirementAllAsset = dataRequirementToAssetMap[aiDataAsset.dataAssetInfo.dataRequirementID];
+        requirementAllAsset.push(assetID);
+        return true;
+        
     } 
 
  
@@ -71,9 +83,11 @@ contract AIDataAssetRegister {
         dataExchangeAddr = addr;
     }
 
-    function queryDataExchangeManagerAddr() constant public returns (DataExchangeManager addr) {
-        addr = dataExchangeAddr;
-    }
+    //function queryDataExchangeManagerAddr() constant public returns (DataExchangeManager addr) {
+    //    addr = dataExchangeAddr;
+    //}
+
+
     
 
     function queryAiDataAssetOwner(string  assetID) constant public returns(address owner) {
@@ -90,58 +104,25 @@ contract AIDataAssetRegister {
 
 
 
-    function queryAiAssetbyOwner(address owner) constant public returns (string _json) {
+    
+    function queryAiAssetbyOwnerAndAttribute(address owner, LibAIDataAsset.AssetDataType dataType, string dataRequirementID) constant public returns (string _json) {
          
         string[] owernAllAsset = aiDataAssetAddrMap[owner];
         
-        uint assetTotolNum = owernAllAsset.length;
-        
-        _json = "{";
-        
-
-        _json = _json.concat(assetTotolNum.toKeyValue("totolNum"));
-
-        if(assetTotolNum > 0){
-            _json = _json.concat(", \"items\":[");
-            
-            for(uint i= 0;i < assetTotolNum;i++){
-                if (i>0){
-                    _json = _json.concat(",");
-                }
-                //assetID = owernAllAsset[i];
-
-                LibAIDataAsset.AiDataAssetInfo aiDataAsset = aiDataAssetMap[owernAllAsset[i]];
-                string memory tempJson  = LibAIDataAsset.toJson(aiDataAsset);
-                _json = _json.concat(tempJson);
-            }
-
-            _json = _json.concat("]}");
-
-        }
-        else {
-            _json = _json.concat("}");
-        }       
-
-    }
-
-    function queryAiAssetbyOwnerAndDataType(address owner, LibAIDataAsset.AssetDataType dataType) constant public returns (string _json) {
-         
-        string[] owernAllAsset = aiDataAssetAddrMap[owner];
-        
-        uint assetTotolNum = owernAllAsset.length;
-        
+        uint assetTotalNum = owernAllAsset.length;     
         
         
 
-        //_json = _json.concat(assetTotolNum.toKeyValue("totolNum"));
+        //_json = _json.concat(assetTotalNum.toKeyValue("totalNum"));
         uint coutner = 0;
-        if(assetTotolNum > 0){
+        if(assetTotalNum > 0){
             string memory _jsonTmp = _jsonTmp.concat("\"items\":[");
             
-            for(uint i= 0;i < assetTotolNum;i++){
+            for(uint i= 0;i < assetTotalNum;i++){
 
                 LibAIDataAsset.AiDataAssetInfo aiDataAsset = aiDataAssetMap[owernAllAsset[i]];
-                if( aiDataAsset.aiDataModel.assetDataType != dataType) continue;
+                if ((LibAIDataAsset.AssetDataType.DATATYPE_MAX > dataType) && (aiDataAsset.aiDataModel.assetDataType != dataType)) continue;
+                if ((!dataRequirementID.equals("")) && (( !aiDataAsset.dataAssetInfo.dataRequirementID.equals(dataRequirementID)))) continue;
 
                 if (coutner>0){
                     _jsonTmp = _jsonTmp.concat(",");
@@ -153,24 +134,63 @@ contract AIDataAssetRegister {
             }
 
             _jsonTmp = _jsonTmp.concat("]");
-
         }
-        else {
-            _json = _json.concat("}");
-        }   
  
         _json = "{";
         
         if(coutner>0)    {
-            _json = _json.concat(coutner.toKeyValue("totolNum"), ",");
+            _json = _json.concat(coutner.toKeyValue("totalNum"), ",");
             _json = _json.concat(_jsonTmp);
         }else {
-            _json = _json.concat(coutner.toKeyValue("totolNum"));
+            _json = _json.concat(coutner.toKeyValue("totalNum"));
         }
 
         _json = _json.concat("}");
 
     }
+
+    function queryAssetbyRequirementID(string dataRequirementID) constant public returns(string _json) {
+         
+        string[] requirementAllAsset = dataRequirementToAssetMap[dataRequirementID];
+        
+        uint assetTotalNum = requirementAllAsset.length;           
+
+
+        uint coutner = 0;
+        if(assetTotalNum > 0){
+            string memory _jsonTmp = _jsonTmp.concat("\"items\":[");
+            
+            for(uint i= 0;i < assetTotalNum;i++){
+                
+                if (coutner>0){
+                    _jsonTmp = _jsonTmp.concat(",");
+                }
+
+                LibAIDataAsset.AiDataAssetInfo aiDataAsset = aiDataAssetMap[requirementAllAsset[i]];
+                                
+                string memory tempJson  = LibAIDataAsset.toJson(aiDataAsset);
+                _jsonTmp = _jsonTmp.concat(tempJson);
+                coutner++;
+            }
+
+            _jsonTmp = _jsonTmp.concat("]");
+
+        }
+       
+ 
+        _json = "{";
+        
+        if(coutner>0)    {
+            _json = _json.concat(coutner.toKeyValue("totalNum"), ",");
+            _json = _json.concat(_jsonTmp);
+        }else {
+            _json = _json.concat(coutner.toKeyValue("totalNum"));
+        }
+
+        _json = _json.concat("}");
+
+    }
+
 
     /*
     function querybyStatus() constant {
