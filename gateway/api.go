4407be405ca22f135a34d882f4bbf0b78a6a0b78
Patrick Star、 <778774780@qq.com>
2018-02-05 14:36:34 +0800

    micro service

---
+++ gateway/api.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	proto "../service/identity/proto"
+	"github.com/micro/go-micro/client"
+	//"github.com/micro/go-micro/cmd"
+	"golang.org/x/net/context"
+	"github.com/micro/go-web"
+	"net/http"
+)
+
+func main() {
+	service := web.NewService(
+		web.Name("go.micro.api.user"),
+	)
+
+	service.HandleFunc("/register", register)
+	service.HandleFunc("/login", login)
+
+
+	if err := service.Init(); err != nil {
+		fmt.Print(err)
+	}
+
+	if err := service.Run(); err != nil {
+		fmt.Print(err)
+	}
+}
+func login(writer http.ResponseWriter, request *http.Request) {
+	//cmd.Init()
+
+	// Use the generated client stub
+	cl := proto.NewUserClient("go.micro.srv.user", client.DefaultClient)
+
+	// Make request
+	rsp, err := cl.Login(context.Background(), &proto.LoginRequest{
+		Username: request.FormValue("userName"),
+		Signature: request.FormValue("signature"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprint(writer, rsp.Msg, rsp.Token)
+}
+func register(writer http.ResponseWriter, request *http.Request) {
+	//cmd.Init()
+
+	// Use the generated client stub
+	cl := proto.NewUserClient("go.micro.srv.user", client.DefaultClient)
+
+	// Make request
+	rsp, err := cl.Register(context.Background(), &proto.RegisterRequest{
+		Username: request.FormValue("userName"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprint(writer, rsp.Msg)
+}
---
+++ service/identity/main.go
@@ -0,0 +1,48 @@
+package identity
+
+import (
+	"log"
+	"github.com/micro/go-micro"
+	 proto "./proto"
+	"golang.org/x/net/context"
+	"github.com/emicklei/go-restful"
+)
+
+type User struct{}
+
+func (u *User) Register(ctx context.Context, req *proto.RegisterRequest, rsp *proto.RegisterResponse) error {
+	rsp.Code = 1
+	rsp.Msg = "OK"
+	return nil
+}
+
+func (u *User) Login(ctx context.Context, req *proto.LoginRequest, rsp *proto.LoginResponse) error {
+	rsp.Code = 1
+	rsp.Msg = "OK"
+	rsp.Token = req.Username + req.Signature
+	return nil
+}
+
+
+func WriteChain(req *restful.Request, rsp *restful.Response) error {
+	log.Print("Received Say.Anything API request")
+	rsp.WriteEntity(map[string]string{
+		"message": "Hi, this is the Greeter API",
+	})
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("go.micro.srv.user"),
+		micro.Version("2.0.0"),
+	)
+
+	service.Init()
+
+	proto.RegisterUserHandler(service.Server(), new(User))
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
