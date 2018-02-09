36fd18ae43715d5c936c460f8594aa551f58c573
luomeiqin <maypen890@aliyun.com>
2018-02-09 09:51:00 +0800

    [all]update storage

--- service/storage/internal/platform/config/version.go
+++
@@ -1,8 +0,0 @@
-package config
-
-// Version is used to define the application version and should be overridden during the
-// compilation phase.
-var Version = "(undefined)"
-
-// ServiceName indicates the discovery identity for this service
-const ServiceName = "storemanagement"
