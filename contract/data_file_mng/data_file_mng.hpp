803158f8be45d7b7145dce7cb008f6f13543c5da
bottoszw <zhangwei@bottos.org>
2018-03-03 16:46:42 +0800

    add struct define head file of data file register

---
+++ contract/data_file_mng/data_file_mng.hpp
@@ -0,0 +1,29 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include <eoslib/eos.hpp>
+#include <eoslib/string.hpp>
+
+/* @abi action login
+ * @abi table
+*/
+struct data_file_info {
+    eosio::string user_name;
+    eosio::string session_id;
+    uint32_t      file_size;
+    eosio::string file_name;
+    eosio::string file_policy;
+    uint32_t      file_number;
+    eosio::string signature;
+};
+
+
+struct reg_data_file_req {
+    eosio::string  file_hash;
+    data_file_info info;
+};
+
+
+
+
