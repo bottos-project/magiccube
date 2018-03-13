f4843a28e08c66bdc2663161df4b2c55bc55cc27
luomeiqin <maypen890@aliyun.com>
2018-03-13 10:39:43 +0800

    [storage]update httphandler

--- service/storage/controller/httphandler_test.go
+++ service/storage/controller/httphandler_test.go
@@ -1,39 +1,38 @@
 package controller
 
 import (
-	"testing"
 	"fmt"
+	"testing"
+
 	"github.com/code/bottos/service/storage/util"
 )
 
-
 func TestGetInfo(t *testing.T) {
 	var rsp *util.Info
-	rsp, _= GetInfo()
+	rsp, _ = GetInfo()
 	fmt.Println(rsp.HeadBlockNum)
 	fmt.Println(rsp.ServerVersion)
 }
 func TestGetBlock(t *testing.T) {
 	var rsp *util.Block
-	rsp, _= GetBlock("0000000445a9f27898383fd7de32835d5d6a978cc14ce40d9f327b5329de796b")
+	rsp, _ = GetBlock("0000000445a9f27898383fd7de32835d5d6a978cc14ce40d9f327b5329de796b")
 	fmt.Println(rsp.Producer)
 	fmt.Println(rsp.ID)
 }
 func TestGetBlockNum1(t *testing.T) {
 	var rsp *util.Block
-	rsp, _= GetBlock("1")
+	rsp, _ = GetBlock("1")
 	fmt.Println(rsp.Producer)
 	fmt.Println(rsp.ID)
 }
 func TestGetAccountInfo(t *testing.T) {
 	var rsp *util.AccountInfo
-	rsp, _= GetAccountInfo()
+	rsp, _ = GetAccountInfo()
 	fmt.Println(rsp.AccountName)
-	fmt.Println(rsp.EosBalance)
 }
 func TestGetTxInfo(t *testing.T) {
 	var rsp *util.TxInfo
-	rsp, _= GetTxInfo()
+	rsp, _ = GetTxInfo()
 	fmt.Println(rsp.TransactionID)
 	fmt.Println(rsp.Transaction.Expiration)
 }
