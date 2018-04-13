ea9b25a5f21c668f8d44511153365b0d05e9b340
huaapp <jiawang06@163.com>
2018-04-13 14:47:42 +0800

    update config file

--- config/base.go
+++ config/base.go
@@ -1,10 +1,25 @@
-package config
-
+﻿package config
 
 const (
-	BASE_URL    	= "http://47.98.47.148:8887/"
-	//BASE_URL    	= "http://127.0.0.1:8888/"
-	BASE_RPC		= "http://47.98.47.148:8080/rpc"
-	BASE_LOG_CONF	= "config/log.json"
-)
+	BASE_CHAIN_IP   = "127.0.0.1"
+	BASE_CHAIN_PORT = "8689"
+	BASE_CHAIN_URL  = "http://" + BASE_CHAIN_IP + ":" + BASE_CHAIN_PORT + "/"
+	BASE_RPC        = "http://" + BASE_CHAIN_IP + ":8080/rpc"
+	//BASE_RPC        		= "http://127.0.0.1:8080/rpc"
 
+	BASE_MONGODB_PORT = "27017"
+	BASE_MONGODB_ADDR = BASE_CHAIN_IP + ":" + BASE_MONGODB_PORT
+	DB_NAME           = "bottos"
+
+	BASE_MINIO_IP         = "xx"
+	BASE_MINIO_PORT       = "9000"
+	BASE_MINIO_ADDR       = BASE_MINIO_IP + ":" + BASE_MINIO_PORT
+	BASE_MINIO_ACCESS_KEY = ""
+	BASE_MINIO_SECRET_KEY = ""
+
+	BASE_LOG_CONF	= "config/log.json"
+	
+
+	
+	TOKEN_EXPIRE_TIME = 2 * 60 * 60
+)
