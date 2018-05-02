f563b1cd7f68261e9a3c06cd0cc6b2bbe9402c50
lyp830414 <lyp830414@163.com>
2018-05-02 06:50:51 -0700

    [Node] add config for node

---
+++ service/node/config/config.go
@@ -0,0 +1,20 @@
+package config
+
+const(
+	//CONFIG_FILE = "C:\\Users\\stewa\\go\\src\\github.com\\code\\bottos\\service\\node\\config\\config.json"
+	CONFIG_FILE = "/opt/go/bin/config.json"
+	//SERV_IP="10.104.14.169"
+	//SERV_PORT="8888"
+	//SERV_URL=SERV_IP+":"+SERV_PORT
+	WALLET_IP="10.104.14.184"
+
+	//RUN_PLATFORM="windows"
+	RUN_PLATFORM="linux"
+
+	SCRIPT_PATH="/opt/go/bin/build.sh"
+	//KEYSTONE_PATH="/home/bot/keystone.bto"
+        
+        MONGO_DB_URL = "127.0.0.1:27017"
+        DB_BOTTOS = "local"
+        TABLE_POINTXY = "pointxy"
+)
