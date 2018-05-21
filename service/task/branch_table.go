4fb4233ede47adddfc52e7207571122f0a73c817
Patrick Star、 <778774780@qq.com>
2018-05-21 09:32:18 +0800

    branch table

--- service/task/branch_table.go
+++ service/task/branch_table.go
@@ -4,6 +4,7 @@ import(
 	log "github.com/cihub/seelog"
 	"github.com/bottos-project/bottos/tools/db/mongodb"
 	"gopkg.in/mgo.v2/bson"
+	"time"
 )
 
 var branch_table = []string{"favoritepro", "datareqreg", "assetreg"}
@@ -33,17 +34,18 @@ type RecMessageId struct {
 }
 
 type Tx struct {
-	ID          bson.ObjectId `bson:"_id,omitempty"`
-	Version     uint32  `bson:"version"`
-	CursorNum   uint32  `bson:"cursor_num"`
-	CursorLabel uint32  `bson:"cursor_label"`
-	Lifetime    uint64  `bson:"lifetime"`
-	Sender      string  `bson:"sender"`
-	Contract    string  `bson:"contract"`
-	Method      string  `bson:"method"`
-	Param       interface{}  `bson:"param"`
-	SigAlg      uint32  `bson:"sig_alg"`
-	Signature   string  `bson:"signature"`
+	ID          bson.ObjectId 	`bson:"_id,omitempty"`
+	Version     uint32  		`bson:"version"`
+	CursorNum   uint32  		`bson:"cursor_num"`
+	CursorLabel uint32  		`bson:"cursor_label"`
+	Lifetime    uint64  		`bson:"lifetime"`
+	Sender      string  		`bson:"sender"`
+	Contract    string  		`bson:"contract"`
+	Method      string  		`bson:"method"`
+	Param       interface{}  	`bson:"param"`
+	SigAlg      uint32  		`bson:"sig_alg"`
+	Signature   string  		`bson:"signature"`
+	CreateTime  time.Time		`bson:"create_time"`
 }
 
 func init() {
