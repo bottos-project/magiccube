2aa39a858b40a3cfead8933daf1659070292cd07
huaapp <jiawang06@163.com>
2018-05-23 10:48:15 +0800

    add presale

--- service/asset/assApi/main.go
+++ service/asset/assApi/main.go
@@ -351,6 +351,32 @@ func (u *Asset) GetUserPurchaseAssetList(ctx context.Context, req *api.Request, 
 	return nil
 }*/
 
+func (s *Asset) PreSaleNotice(ctx context.Context, req *api.Request, rsp *api.Response) error {
+	rsp.StatusCode = 200
+
+	//验签
+	is_true, err := sign.PushVerifySign(req.Body)
+	if !is_true {
+		rsp.Body = errcode.ReturnError(1000, err)
+		return nil
+	}
+
+	var publishRequest asset.PushTxRequest
+	err = json.Unmarshal([]byte(req.Body), &publishRequest)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	response, err := s.Client.PreSaleNotice(ctx, &publishRequest)
+	if err != nil {
+		return err
+	}
+
+	rsp.Body = errcode.Return(response)
+	return nil
+}
+
 func (u *Asset) QueryMyNotice(ctx context.Context, req *api.Request, rsp *api.Response) error {
 	rsp.StatusCode = 200
 	body := req.Body
