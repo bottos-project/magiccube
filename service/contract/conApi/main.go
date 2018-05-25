b81793c670a1d530f1b6015c7768c68cc4fb4628
Patrick Star、 <778774780@qq.com>
2018-05-25 18:23:07 +0800

    contract module

--- service/contract/conApi/main.go
+++ service/contract/conApi/main.go
@@ -8,6 +8,12 @@ import (
 	api "github.com/micro/micro/api/proto"
 	"golang.org/x/net/context"
 	"os"
+	"strings"
+	"io/ioutil"
+	"net/http"
+	"github.com/bottos-project/magiccube/service/common/data"
+	"errors"
+	"encoding/json"
 )
 
 type Contract struct {
@@ -15,49 +21,40 @@ type Contract struct {
 }
 
 func (s *Contract) Publish(ctx context.Context, req *api.Request, rsp *api.Response) error {
-	//rsp.StatusCode = 200
-	//
-	////验签
-	//is_true, err := sign.PushVerifySign(req.Body)
-	//if !is_true {
-	//	rsp.Body = errcode.ReturnError(1000, err)
-	//	return nil
-	//}
-	//
-	//var publishRequest contract.PublishRequest
-	//err = json.Unmarshal([]byte(req.Body), &publishRequest)
-	//if err != nil {
-	//	log.Error(err)
-	//	return err
-	//}
-	//
-	//response, err := s.Client.Publish(ctx, &publishRequest)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//rsp.Body = errcode.Return(response)
+
 	return nil
 }
 
 func (s *Contract) Query(ctx context.Context, req *api.Request, rsp *api.Response) error {
-	//rsp.StatusCode = 200
-	//body := req.Body
-	//log.Info(body)
-	//var requirementQuery requirement.QueryRequest
-	//err := json.Unmarshal([]byte(body), &requirementQuery)
-	//if err != nil {
-	//	log.Error(err)
-	//	return err
-	//}
-	//requirementQuery.Username = ""
-	//response, err := s.Client.Query(ctx, &requirementQuery)
-	//if err != nil {
-	//	log.Error(err)
-	//	return err
-	//}
-	//
-	//rsp.Body = errcode.Return(response)
+
+	var queryRequest contract.QueryRequest
+	err := json.Unmarshal([]byte(req.Body), &queryRequest)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	params := `service=core&method=CoreApi.QueryAbi&request={"contract":"`+queryRequest.Contract+`"}`
+	resp, err := http.Post(data.BASE_URL, "application/x-www-form-urlencoded",
+		strings.NewReader(params))
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if (resp.StatusCode != 200) {
+		log.Error(resp.Status)
+		return errors.New(string(body))
+	}
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	rsp.Body = string(body)
 	return nil
 }
 
