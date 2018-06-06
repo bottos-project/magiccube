f99e2d830f59b24e7e35e8c56bde7dc61d8eb9ed
huaapp <jiawang06@163.com>
2018-06-06 10:46:54 +0800

    update node

--- service/storage/util/time-info.go
+++ service/storage/util/time-info.go
@@ -32,15 +32,15 @@ type DayRange struct {
 
 func dayDur(now time.Time) (time.Time, time.Time) {
 
-	today_str := now.Format("20060102")
+	todayStr := now.Format("20060102")
 
-	yesterday_Date := now.AddDate(0, 0, -1)
-	yesterday_str := yesterday_Date.Format("20060102")
+	yesterdayDate := now.AddDate(0, 0, -1)
+	yesterdayStr := yesterdayDate.Format("20060102")
 
-	yesterday_time, _ := time.Parse("20060102", yesterday_str)
-	today_time, _ := time.Parse("20060102", today_str)
+	yesterdayTime, _ := time.Parse("20060102", yesterdayStr)
+	todayTime, _ := time.Parse("20060102", todayStr)
 
-	return yesterday_time, today_time
+	return yesterdayTime, todayTime
 }
 
 //YesterdayDur is to YesterdayDur
